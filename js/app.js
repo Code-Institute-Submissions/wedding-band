25e8aacc8283da6d8c3350dc06e463f563baa283
Conrad Coyle <coylec@users.noreply.github.com>
2017-01-27 17:10:44 +0000

    Added routing for other pages

--- js/app.js
+++ js/app.js
@@ -6,6 +6,26 @@ angular.module("WeddingBand").config(function($locationProvider, $routeProvider)
 	$routeProvider.when("/", {
 		templateUrl: "templates/home.html",
 		controller: "HomeController"
+	})
+	.when("/music", {
+		templateUrl: "templates/music.html",
+		controller: "MusicController"
+	})
+	.when("/setlist", {
+		templateUrl: "templates/setlist",
+		controller: "SetlistController"
+	})
+	.when("/pricing", {
+		templateUrl: "templates/pricing",
+		controller: "PricingController"
+	})
+	.when("/faq", {
+		templateUrl: "templates/faq",
+		controller: "FaqController"
+	})
+	.when("/contact", {
+		templateUrl: "templates/contact",
+		controller: "ContactController"
 	});
 
 });
--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,13 @@
 angular.module("RouteControllers", [])
 	.controller("HomeController", function($scope){
+	})
+	.controller("MusicController", function($scope){
+	})
+	.controller("SetlistController", function($scope){
+	})
+	.controller("PricingController", function($scope){
+	})
+	.controller("FaqController", function($scope){
+	})
+	.controller("ContactController", function($scope){
 	});
