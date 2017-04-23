30a1b1260d7b3293e69ec41d4687167ad9a1367f
Conrad Coyle <coylec@users.noreply.github.com>
2017-04-23 15:57:56 +0100

    Fixed social icons and added comments

--- js/service.js
+++ js/service.js
@@ -1,26 +1,4 @@
-/*(function(){
-	var datesService = function(){
-		var dates = [
-				{month: "January", day: "15th"},
-				{month: "March", day: "24th"},
-				{month: "June", day: "5th"}
-			];
-		
-
-	this.getDate = function() {
-		for (var i=0, len=dates.length; i<len; i++) {
-				return dates[i];
-		}
-		return {};
-	};
-
-};
-
-	angular.module("WeddingBand").service("datesService", datesService);
-}());*/
-
-
-/*Attempted to refactor code above as for loop seemed unnecessary if using ng-repeat*/
+/*Service to serve a list of dates to user when button is clicked*/
 (function(){
 	var datesService = function(){
 		var dates = [
