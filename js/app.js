84ea7902746597a566fd030e25659ba9bda97d8a
Conrad Coyle <coylec@users.noreply.github.com>
2017-04-24 13:51:40 +0100

    Change paths to be relative

--- js/app.js
+++ js/app.js
@@ -3,31 +3,31 @@ angular.module("WeddingBand", ["ngRoute", "RouteControllers", "mgcrea.ngStrap"])
 angular.module("WeddingBand").config(function($locationProvider, $routeProvider){
 	$locationProvider.html5Mode(true);
 
-	$routeProvider.when("/", {
+	$routeProvider.when(".", {
 		templateUrl: "templates/home.html",
 		controller: "HomeController"
 	})
-	.when("/music", {
+	.when("music", {
 		templateUrl: "templates/music.html",
 		controller: "MusicController"
 	})
-	.when("/setlist", {
+	.when("setlist", {
 		templateUrl: "templates/setlist",
 		controller: "SetlistController"
 	})
-	.when("/pricing", {
+	.when("pricing", {
 		templateUrl: "templates/pricing",
 		controller: "PricingController"
 	})
-	.when("/faq", {
+	.when("faq", {
 		templateUrl: "templates/faq",
 		controller: "FaqController"
 	})
-	.when("/contact/contactform", {
+	.when("contact/contactform", {
 		templateUrl: "templates/contact.html",
 		controller: "ContactController"
 	})
-	.when("/contact/dates", {
+	.when("contact/dates", {
 		templateUrl: "templates/dates.html",
 		controller: "DatesController"
 	});
