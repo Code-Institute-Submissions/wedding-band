2c8390bcc400d888df55d49e61542bc5d4fd6285
Conrad Coyle <coylec@users.noreply.github.com>
2017-04-24 15:02:21 +0100

    Possible routing fix

--- js/app.js
+++ js/app.js
@@ -3,23 +3,23 @@ angular.module("WeddingBand", ["ngRoute", "RouteControllers", "mgcrea.ngStrap"])
 angular.module("WeddingBand").config(function($locationProvider, $routeProvider){
 	$locationProvider.html5Mode(true);
 
-	$routeProvider.when(".", {
+	$routeProvider.when("/", {
 		templateUrl: "templates/home.html",
 		controller: "HomeController"
 	})
-	.when("music", {
+	.when("/music", {
 		templateUrl: "templates/music.html",
 		controller: "MusicController"
 	})
-	.when("setlist", {
+	.when("/setlist", {
 		templateUrl: "templates/setlist",
 		controller: "SetlistController"
 	})
-	.when("pricing", {
+	.when("/pricing", {
 		templateUrl: "templates/pricing",
 		controller: "PricingController"
 	})
-	.when("faq", {
+	.when("/faq", {
 		templateUrl: "templates/faq",
 		controller: "FaqController"
 	})
